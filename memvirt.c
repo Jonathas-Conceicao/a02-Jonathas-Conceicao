87226e0cf335db4526ef4d01e38d62052ac45a48
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2017-06-24 13:31:15 -0300

    Empty files to get started

---
+++ memvirt.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "memvirt.h"
+
+struct result * memvirt(int num_procs, uint32_t num_frames, char * filename, uint32_t interval) {
+  (void)num_procs;
+  (void)num_frames;
+  (void)filename;
+  (void)interval;
+
+  return NULL;
+}
---
+++ teste.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "memvirt.h"
+#include "simpletest.h"
+
+int main(int argc, char const *argv[]) {
+  (void)argc; // Essa forma não bagunça o hightlight do meu editor.
+  (void)argv;
+  return 0;
+}
