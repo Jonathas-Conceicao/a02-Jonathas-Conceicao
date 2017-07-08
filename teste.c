7db897e27fcd69d841599da6567e9eafe673c905
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2017-07-07 21:09:42 -0300

    One last test

--- teste.c
+++ teste.c
@@ -12,7 +12,7 @@ void destryResult(struct result** pR) {
 
 void test1(void) {
   WHEN("Teste simples com 12 operações.");
-  IF("Três processos sem recálculo de working set acessando apenas duas páginas, mas com 9 frames no total.");
+  IF("Três processos sem mudança de working set acessando apenas duas páginas, mas com 9 frames no total.");
 
   struct result *ret;
   ret = memvirt(3, 9, "t01.txt", 12);
@@ -40,11 +40,32 @@ void test1(void) {
   return;
 }
 
+void test2(void) {
+  WHEN("Teste com 16 operações.");
+  IF("Dois processos acessando páginas difernetes e com repetição apenas no final.");
+
+  struct result *ret;
+  ret = memvirt(2, 8, "t02.txt", 4);
+
+  THEN("Ambos os processos devem ter 8 acessos.");
+  isEqual(ret->refs[0], 8, 1);
+  isEqual(ret->refs[1], 8, 1);
+  THEN("Ambos devem ter 7 pf.");
+  isEqual(ret->pfs[0], 7, 1);
+  isEqual(ret->pfs[1], 7, 1);
+
+  THEN("Working set médio deve ser 2.");
+  isEqual(ret->avg_ws, 2, 1);
+  THEN("O total de page fault deve ser de 87.5%.");
+  isNear(ret->total_pf_rate, 0.87500, 1);
+}
+
 int main(int argc, char const *argv[]) {
   (void)argc; // Essa forma não bagunça o hightlight do meu editor.
   (void)argv;
   DESCRIBE("Testes do simulador de gerênciamento de meória virtual");
   test1();
+  test2();
   GRADEME();
   return 0;
 }
